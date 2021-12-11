9d982ee37c9f7ac36da2e2729492f7c43f6b907d
firo <cfiro@qq.com>
2021-12-11 13:56:42 +0800

    样式完成

--- src/components/myItem.vue
+++ src/components/myItem.vue
@@ -1,20 +1,31 @@
 <template>
-  <el-collapse-item title="一致性 Consistency" name="1">
-        <div>
-          与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；
-        </div>
-        <div>
-          在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。
-        </div>
-      </el-collapse-item>
+   <div class="shell">
+       <el-checkbox v-model="checked" size="medium"></el-checkbox>
+       <div class="text">xxxxxxxxx</div>
+   </div>
 </template>
 
 <script>
 export default {
  name: "myItem",
+ data() {
+     return {
+         checked:'',
+     }
+ },
 }
 </script>
 
-<style>
+<style scoped>
+.shell{
+    display: flex;
+    width: 100%;
+    height: 49px;
+    border-bottom:1px solid #EBEEF5;
+    line-height: 49px;
+}
+.text {
+    margin-left: 10px;
+}
 
 </style>
